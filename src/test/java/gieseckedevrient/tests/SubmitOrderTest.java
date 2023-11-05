0970d59d2695da8ec6b2d8e87ec80e5ff59d9bfb
Jay Shree Ram <kundadantikurhe1005@gmail.com>
2023-11-05 14:44:07 +0530

    code for selenium framework

---
+++ src/test/java/gieseckedevrient/tests/SubmitOrderTest.java
@@ -0,0 +1,96 @@
+package gieseckedevrient.tests;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import gieseckeanddevrient.TestComponents.BaseTest;
+import gieseckeanddevrient.pageobjects.CartPage;
+import gieseckeanddevrient.pageobjects.CheckoutPage;
+import gieseckeanddevrient.pageobjects.ConfirmationPage;
+import gieseckeanddevrient.pageobjects.LandingPage;
+import gieseckeanddevrient.pageobjects.OrderPage;
+import gieseckeanddevrient.pageobjects.ProductCatalogue;
+
+import org.testng.AssertJUnit;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SubmitOrderTest extends BaseTest {
+	String productName = "ZARA COAT 3";
+
+	@Test(dataProvider = "getData", groups = { "Purchase" })
+	public void submitOrder(HashMap<String, String> input) throws IOException, InterruptedException {
+		ProductCatalogue productCatalogue = landingPage.loginApplication(input.get("email"), input.get("password"));
+		List<WebElement> products = productCatalogue.getProductList();
+		productCatalogue.addProductToCart(input.get("product"));
+		CartPage cartPage = productCatalogue.goToCartPage();
+
+		Boolean match = cartPage.VerifyProductDisplay(input.get("product"));
+		Assert.assertTrue(match);
+		CheckoutPage checkoutPage = cartPage.goToCheckout();
+		checkoutPage.selectCountry("india");
+		ConfirmationPage confirmationPage = checkoutPage.submitOrder();
+		String confirmMessage = confirmationPage.getConfirmationMessage();
+		Assert.assertTrue(confirmMessage.equalsIgnoreCase("THANK YOU FOR THE ORDER."));
+	}
+
+	@Test(dependsOnMethods = { "submitOrder" })
+	public void OrderHistoryTest() {
+		// "ZARA COAT 3";
+		ProductCatalogue productCatalogue = landingPage.loginApplication("anshika@gmail.com", "Iamking@000");
+		OrderPage ordersPage = productCatalogue.goToOrdersPage();
+		Assert.assertTrue(ordersPage.VerifyOrderDisplay(productName));
+	}
+	
+	public String getScreenshot(String testCaseName) throws IOException {
+		TakesScreenshot ts = (TakesScreenshot)driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		File file = new File(System.getProperty("user.dir") + "//reports//" + testCaseName + ".png");
+		FileUtils.copyFile(source, file);
+		return System.getProperty("user.dir") + "//reports//" + testCaseName + ".png";
+	}
+
+	// Extent Reports -
+	@DataProvider
+	public Object[][] getData() throws IOException {
+		List<HashMap<String, String>> data = getJsonDataToMap(
+				System.getProperty("user.dir") + "/src/test/java/gieseckedevrient/data/PurchaseOrder.json");
+		return new Object[][] { { data.get(0) }, { data.get(1) } };
+	}
+
+//	 @DataProvider
+//	  public Object[][] getData()
+//	  {
+//	    return new Object[][]  {{"anshika@gmail.com","Iamking@000","ZARA COAT 3"}, {"shetty@gmail.com","Iamking@000","ADIDAS ORIGINAL" } };
+//	    
+//	  }
+//	HashMap<String,String> map = new HashMap<String,String>();
+//	map.put("email", "anshika@gmail.com");
+//	map.put("password", "Iamking@000");
+//	map.put("product", "ZARA COAT 3");
+//	
+//	HashMap<String,String> map1 = new HashMap<String,String>();
+//	map1.put("email", "shetty@gmail.com");
+//	map1.put("password", "Iamking@000");
+//	map1.put("product", "ADIDAS ORIGINAL");
+	
+}
